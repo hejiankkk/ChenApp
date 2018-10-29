d5e6dcd94245686dc36eab4bd7b718859eb6b1f5
hejian <403097365@qq.com>
2018-10-29 22:39:51 +0800

    init

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ChenApp.Models;
+using ChenApp.Services;
+
+namespace ChenApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IUserInfoService _userInfoService;
+
+        public HomeController(IUserInfoService userInfoService)
+        {
+            _userInfoService = userInfoService;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.User = _userInfoService.GetUserInfo();
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using ChenApp.Infrastructure.Core;
+
+namespace ChenApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Engine.Instance.Initialize();
+
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
